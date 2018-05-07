7651fcbb908685a88c413a6c9b5f4dcfc0ccb1a2
sameer <jasitis@gmail.com>
2018-05-07 11:50:58 +0530

    .

---
+++ letter.mm
@@ -0,0 +1,80 @@
+.WA "Christopher T. Craft"
+999 Anyway Way
+Anytown, UT 84111 USA
+.WE
+.IA
+John W. Doe
+111 Notown Blvd.
+Notown, UT 84111
+.IE
+.LO RN "Our telephone conversation"
+.LO SA "Dear Mr. Doe:"
+.LT
+In reference to our telephone conversation on the 4th, I am calling to
+confirm our upcoming appointment on the 18th. I look forward to
+discussing the merger. I believe we have a win-win situation here.
+.FC "Yours Truly,"
+.SG
+.TL
+Merger Technical Specifications
+.AF "ABC Corporation"
+.AU "Christopher Craft"
+.AT "President"
+.AS
+This memo details the specifications for the planned merger.
+.AE
+.MT "Merger Description and Marching Orders"
+As a result of our talks with XYZ corporation, we plan to go
+forward with the merger. This document contains the following:
+.BL
+.LI
+Schedule and time tables.
+.LI
+Financial statements.
+.LI
+Asset allocations.
+.LE
+.SP
+Please add any corrections you have, then sign the approval line
+indicated at the bottom of this sheet.
+.FC
+.SG
+.AV "John W. Doe, XYZ Corporation President"
+.AV "Sylvia Q. Public, XYZ Corporation CFO"
+.NS
+Everyone in the corporation.
+.NE
+.EQ
+a ~ mark = ~ 30
+.EN
+.sp
+.EQ
+a sup 2 ~ + ~ b sup 2~lineup = ~ 1000
+.EN
+.sp
+.EQ
+x sup 3 ~ + ~ y sup 3 ~ + ~ z sup 3~lineup = ~ 1400
+.EN
+.TS
+center, box, tab(:);
+c s s
+c | c | c
+l | l | l.
+Mergers and Acquisitions Team
+=
+Employee:Title:Location
+=_
+Jones, James:Marketing Manager:New York Office
+Smith, Charles:Sales Manager:Los Angeles Office
+Taylor, Sarah:R&D Manager:New York Office
+Walters, Mark:Information Systems Manager:Salt Lake City Office
+Zur, Mike:Distribution Manager:Portland Office
+.TE
+.PS
+box invis "Start" "Here"; arrow
+box "Step 1"; arrow
+circle "Step 2"; arrow
+ellipse "Step 3"; arrow
+box "Step 4"; arrow
+box invis "End"
+.PE
---
+++ x.mm
@@ -0,0 +1,9 @@
+.TL
+Simple Sample Document
+.PP
+These are a few lines of sample text.
+It doesn't matter that there is no initial
+indentation or that the lines are both
+long and short,
+because nroff or troff
+will take care of that later.
